5b2baf02546d656ce1ccc75dc6e855e1eedfed3e
Mohammed Afhan <Mohammed.Afhan@fnf.com>
2021-09-06 17:06:19 +0530

    Day1Assignment1

---
+++ Day1Assignments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kelvin, fahrenheit;
+
+            Console.WriteLine("Enter Celsius");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            kelvin = celsius + 273;
+
+            Console.WriteLine($"{celsius} celsius = {kelvin} Kelvin");
+            fahrenheit = (celsius * 18) / 10 + 32;
+
+            Console.WriteLine($"{celsius} celsius = {fahrenheit} Fahrenheit");
+        }
+    }
+}
