e8bcc630f861fa944a257be8f2f75aad8bf37c8c
Mohammed-Afhan <mohammed.afhan@fnf.com>
2021-09-08 22:38:51 +0530

    Add files via upload

---
+++ Day03/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Day1Assignments.Day02;
+
+namespace Day1Assignments.Day03
+{
+    class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public int EmpDOB { get; set; }
+
+
+        public static void CreateNewEmployee()
+        {
+            var EmpId = MyConsole.GetNumber("Enter the ID");
+            Console.WriteLine(EmpId);
+
+            var EmpName = MyConsole.GetString("Enter Employee Name");
+            Console.WriteLine(EmpName);
+
+            var EmpAddress = MyConsole.GetString("Enter Employee Address");
+            Console.WriteLine(EmpAddress);
+
+            Console.WriteLine("Enter the Employee DOB");
+            var EmpDOB = Console.ReadLine();
+            Console.WriteLine(EmpDOB);
+        }
+
+        static void Main()
+        {
+            CreateNewEmployee();
+           
+
+        }
+
+    }
+
+  
+   
+
+
+}
+
+
